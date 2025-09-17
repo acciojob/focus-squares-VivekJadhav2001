234543c444c87032cfc570ff2a80e5303f1072a4
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-17 18:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square")
+
+        squares.forEach((square, index) => {
+            square.addEventListener("mouseover", () => {
+                squares.forEach((s, i) => {
+                    if (i !== index) {
+                        s.style.backgroundColor = "#6F4E37"; // Coffee
+                    }
+                });
+            })
+
+            square.addEventListener("mouseout", () => {
+                squares.forEach(s => {
+
+                    s.style.backgroundColor = "#E6E6FA"; // Lavender
+                }
+                )
+            })
+        })
